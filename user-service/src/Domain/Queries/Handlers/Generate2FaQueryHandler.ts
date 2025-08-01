8d79b4984880351835b54a78d1f67911c912bc06
Bruno Onissanti <brunoonissanti@gmail.com>
2025-08-01 20:59:21 -0300

    feat: add Generate2FaQuery and enhance error handling for 2FA
    - Introduced `Generate2FaQuery` for handling 2FA query logic.
    - Added `Generate2FaDTO` to standardize data transfer.
    - Updated `Generate2FaQueryHandler` to utilize `Generate2FaQuery` and improved property handling.
    - Added `TwofaNotEnabled` error to `ErrorCatalog` for better 2FA error management.

--- user-service/src/Domain/Queries/Handlers/Generate2FaQueryHandler.ts
+++ user-service/src/Domain/Queries/Handlers/Generate2FaQueryHandler.ts
@@ -4,6 +4,7 @@ import {BaseHandlerQuery} from "./BaseHandlerQuery.js";
 import {NotificationError} from "../../../Shared/Errors/NotificationError.js";
 import {UserRepository} from "../../../Infrastructure/Persistence/Repositories/Concrete/UserRepository.js";
 import {Generate2FaQueryDTO} from "../../QueryDTO/Generate2FaQueryDTO.js";
+import {Generate2FaQuery} from "../QueryObject/Generate2FaQuery.js";
 
 export class Generate2FaQueryHandler implements BaseHandlerQuery<Generate2FaQuery, Generate2FaQueryDTO | null>
 {
@@ -13,7 +14,7 @@ export class Generate2FaQueryHandler implements BaseHandlerQuery<Generate2FaQuer
 
     async Handle(query: Generate2FaQuery): Promise<Generate2FaQueryDTO | null>
     {
-        const user = await this.UserRepository.GetUserEntityByUuid(query.Uuid);
+        const user = await this.UserRepository.GetUserEntityByUuid(query.uuid);
 
         if (!user)
             return null;
